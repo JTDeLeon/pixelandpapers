d197f2d524656789a2fe0fb16e2cfec4fa16263c
Jonathan Deleon <Jondeleon856@gmail.com>
2020-01-30 19:37:40 -0500

    structure contact page

---
+++ app/public/wp-content/themes/pixelGenesis/content/contactPage.php
@@ -0,0 +1,76 @@
+<?php /**
+ * Template Name: Contact Us
+ * 
+ * Pixel&Papers
+ * 
+ *
+ * @author     Jonathan Deleon <Jondeleon856@gmail.com>
+ */
+
+ ?>
+
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+    <head>
+        <meta charset="<?php bloginfo('charset'); ?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="profile" href="http://gmpg.org/xfn/11">
+        <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+       
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/content/build/css/style.min.css" type="text/css">
+
+        <!-- wp_head -->
+        <?php wp_head(); ?>
+        <!-- /wp_head -->
+    </head> 
+    <body class="_ox-globals contact " >
+        <div id="page" class="hfeed site">
+            <div id="content" class="site-content" tabindex="-1">
+                <div class="col-full">
+                    <!-- START primary -->
+                    <div id="primary" class="content-area">
+                        <!-- START main -->    
+                        <main id="main" class="site-main" role="main">
+                            <!-- START Page Build -->
+                            <section class="background-section">
+                                <!-- Header Goes Here -->
+
+                                <div class="centeredtxt">
+                                    <h1 class="headlinetxt">Get in touch</h1>
+                                    <p>Want to find out how Pixel and Papers can solve problems specific to your business? Let's talk.</p>
+                                </div>
+
+                                <div class="flex">
+                                    <div class="left">
+                                        <div class="contactform">
+                                            <!-- www.123formbuilder.com script begins here --><script type="text/javascript" defer src="//www.123formbuilder.com/embed/5268400.js" data-role="form" data-default-width="650px"></script><!-- www.123formbuilder.com script ends here -->
+                                        </div>
+                                    </div>
+                                    <div class="right">
+                                        <h3>Other ways to connect</h3>
+                                        <p>Call, email, send us a postcard — whatever works for you. We'll be here.</p>
+                                        <div>
+                                            <p>Phone</p>
+                                            <p>Email</p>
+                                            <p>Address</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </section>
+                            <!-- END Page Build -->
+                        </main>
+                        <!-- END main -->
+                    </div>
+                    <!-- END primary -->
+                </div><!-- .col-full -->
+            </div><!-- #content -->
+        </div><!-- #page -->
+
+        <!-- wp_footer -->
+        <?php wp_footer(); ?>
+        <!-- /wp_footer -->
+    </body>
+
+</html>
