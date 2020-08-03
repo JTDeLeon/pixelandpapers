6c79540ff4472614e247f3b6c74eecdfd7bad83a
Jonathan <jondeleon856@gmail.com>
2020-08-03 19:19:03 -0400

    update new homepage

---
+++ app/public/wp-content/themes/pixelGenesis/content/new-home8-3-20.php
@@ -0,0 +1,362 @@
+<?php /**
+ * Template Name: New Pixel & Papers Homepage
+ * 
+ * Pixel&Papers
+ * 
+ *
+ * @author     Jonathan Deleon <Jondeleon856@gmail.com> & Clay Skougard <>
+ */
+
+ ?>
+
+<!DOCTYPE html>
+<html >
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="generator" content="Mobirise v4.8.1, mobirise.com">
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+  <link rel="shortcut icon" href="wp-content/themes/pixelGenesis/content/assets/images/favicon-122x113.png" type="image/x-icon">
+  <meta name="description" content="">
+  <title>Home</title>
+  <link rel="stylesheet" href="wp-content/themes/pixelGenesis/content/assets/web/assets/mobirise-icons/mobirise-icons.css">
+  <link rel="stylesheet" href="wp-content/themes/pixelGenesis/content/assets/tether/tether.min.css">
+  <link rel="stylesheet" href="wp-content/themes/pixelGenesis/content/assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="wp-content/themes/pixelGenesis/content/assets/bootstrap/css/bootstrap-grid.min.css">
+  <link rel="stylesheet" href="wp-content/themes/pixelGenesis/content/assets/bootstrap/css/bootstrap-reboot.min.css">
+  <link rel="stylesheet" href="wp-content/themes/pixelGenesis/content/assets/dropdown/css/style.css">
+  <link rel="stylesheet" href="wp-content/themes/pixelGenesis/content/assets/animatecss/animate.min.css">
+  <link rel="stylesheet" href="wp-content/themes/pixelGenesis/content/assets/socicon/css/styles.css">
+  <link rel="stylesheet" href="wp-content/themes/pixelGenesis/content/assets/theme/css/style.css">
+  <link rel="stylesheet" href="/wp-content/themes/pixelGenesis/content/build/css/style.css">
+  <link rel="stylesheet" href="wp-content/themes/pixelGenesis/content/assets/mobirise/css/mbr-additional.css" type="text/css">
+  <link rel="stylesheet" href="wp-content/themes/pixelGenesis/content/build/css/style.min.css" type="text/css">
+  
+  
+  
+</head>
+<body>
+    <?php include 'partials/pixelheader.php' ?>
+
+<!-- <section class="engine"><a href="https://mobiri.se/t">amp template</a></section><section class="cid-rEn7ozsbYp mbr-fullscreen mbr-parallax-background" id="header2-0">
+
+    
+
+    
+
+    <div class="container align-center">
+        <div class="row justify-content-md-center">
+            <div class="mbr-white col-md-10">
+                
+                
+                
+                
+            </div>
+        </div>
+    </div>
+    <div class="mbr-arrow hidden-sm-down" aria-hidden="true">
+        <a href="#next">
+            <i class="mbri-down mbr-iconfont"></i>
+        </a>
+    </div>
+</section> -->
+
+<section class="form-section" >
+    <div class="col">
+        <div class="left">
+            <h1>Need a Great Website?  Let’s Talk.</h1>
+            <p class="green-txt">Your website is generally the first opportunity you have to make a lasting impression. We can help you attract new customers, grow your business and carry out your vision for your business.</p>
+        </div>
+        <div class="right">
+            <!-- www.123f ormbuilder.com script begins here --><script type="text/javascript" defer src="//www.123formbuilder.com/embed/5580800.js" data-role="form" data-default-width="650px"></script><!-- www.123formbuilder.com script ends here -->
+        </div>
+    </div>
+    
+</section>
+<section class="mbr-section article content10 cid-rEn84VDMI6" id="content10-1">
+    
+     
+
+    <div class="container mission-section">
+        <div class="inner-container" style="width: 72%;">
+            <hr class="line" style="width: 69%;">
+            <div class="section-text align-center mbr-white mbr-fonts-style display-1">Our mission is to help businesses drive revenue through innovative web design and digital marketing strategy that outshines the competition</div>
+            <hr class="line" style="width: 69%;">
+        </div>
+    </div>
+
+    <?php //include 'partials/email-form.php' ?>
+</section>
+
+<section class="header5 cid-rEF3N6Rfkz mbr-fullscreen mbr-parallax-background understand-section" id="header5-4">
+
+    
+
+    <div class="mbr-overlay" style="opacity: 0.7; background-color: rgb(0, 0, 0);">
+    </div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="mbr-white col-md-10">
+                <h1 class="mbr-section-title align-center pb-3 mbr-fonts-style display-2">All businesses need a modern website where the brand can come to life.</h1>
+                <p class="mbr-text align-center display-5 pb-3 mbr-fonts-style">
+                Here at Pixel & Papers, we are dedicated to creating a unique value around your branding and an optimal digital experience for your customers.  Our pride lies in getting you on track to achieve the greatest return on investment for your business.</p>
+                <br>
+                <p class="mbr-text align-center display-5 pb-3 mbr-fonts-style"><u>Here are a few website design services that we offer:</u>
+                </p>
+                <br>
+                <div class="service-sec">
+                    <div class="container-service">
+                        <h1 class="mbr-section-title align-center pb-3 mbr-fonts-style display-2">Web Design</h1>
+                        <p class="mbr-text align-center display-5 pb-3 mbr-fonts-style">The websites we create combine strategy, design, copywriting and technology to become one of the most influential experiences customers have with your brand.  We match your vision with our skills.
+                        </p>
+                    </div>
+                    <div class="container-service">
+                        <h1 class="mbr-section-title align-center pb-3 mbr-fonts-style display-2">Increase sales</h1>
+                    
+                        <p class="mbr-text align-center display-5 pb-3 mbr-fonts-style">Well designed websites can differentiate your brand from that of competitors. Even without selling directly, great websites become sales tools that inspire action. 
+                        </p>
+                    </div>
+                    <div class="container-service">
+                        <h1 class="mbr-section-title align-center pb-3 mbr-fonts-style display-2">Generate qualified leads</h1>
+                        <p class="mbr-text align-center display-5 pb-3 mbr-fonts-style">Websites are an essential tool for any business’s digital marketing. When built strategically, they become a channel that can generate a steady stream of leads. We carry this approacj into every website we create for clients. 
+                        </p>
+                    </div>
+                </div>
+                <!-- <p class="mbr-text align-center display-5 pb-3 mbr-fonts-style"></p> -->
+                
+            </div>
+        </div>
+    </div>
+
+    
+</section>
+
+<section class="header12 cid-rEF6YoJS5q mbr-fullscreen" id="header12-6">
+
+    
+
+    
+
+    <div class="container  ">
+            <div class="media-container">
+                <div class="col-md-12 align-center">
+                    <h1 class="mbr-section-title pb-3 mbr-white mbr-bold mbr-fonts-style display-1"></h1>
+                    <p class="mbr-text pb-3 mbr-white mbr-fonts-style display-1">We are a marketing and web design agency that combines creativity and business strategy to make brands connect with people.</p>
+                    
+
+                    <div class="icons-media-container mbr-white">
+                        <div class="card col-12 col-md-6 col-lg-3">
+                            <div class="icon-block">
+                            <a href="https://mobirise.com/">
+                                <span class="mbr-iconfont mbri-image-gallery" style="color: rgb(255, 255, 255);"></span>
+                            </a>
+                            </div>
+                            <h5 class="mbr-fonts-style display-5">Marketing</h5>
+                        </div>
+
+                        <div class="card col-12 col-md-6 col-lg-3">
+                            <div class="icon-block">
+                                
+                                    <span class="mbr-iconfont mbri-idea" style="color: rgb(255, 255, 255);"></span>
+                                
+                            </div>
+                            <h5 class="mbr-fonts-style display-5">Web Design</h5>
+                        </div>
+
+                        <div class="card col-12 col-md-6 col-lg-3">
+                            <div class="icon-block">
+                                
+                                    <span class="mbr-iconfont mbri-users" style="color: rgb(255, 255, 255);"></span>
+                                
+                            </div>
+                            <h5 class="mbr-fonts-style display-5">Consulting</h5>
+                        </div>
+
+                        <div class="card col-12 col-md-6 col-lg-3">
+                            <div class="icon-block">
+                                
+                                    <span class="mbr-iconfont mbri-devices" style="color: rgb(255, 255, 255);"></span>
+                                
+                            </div>
+                            <h5 class="mbr-fonts-style display-5">A/B testing</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </div>
+
+    
+</section>
+
+<section class="header5 cid-rEFhx16eAx mbr-fullscreen mbr-parallax-background" id="header5-7">
+
+    
+
+    <div class="mbr-overlay" style="opacity: 0.6; background-color: rgb(0, 0, 0);">
+    </div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="mbr-white col-md-10">
+                <h1 class="mbr-section-title align-center pb-3 mbr-fonts-style display-1">We've had the pleasure to represent these amazing companies.</h1>
+                
+                
+            </div>
+        </div>
+    </div>
+
+    
+</section>
+
+<section class="mbr-section form1 cid-rEFiEm4Mge" id="form1-8">
+
+    
+
+    
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="title col-12 col-lg-8">
+                <h2 class="mbr-section-title align-center pb-3 mbr-fonts-style display-2">
+                    We'd love to hear from you.
+                </h2>
+                <div class="btn-cont">
+                    <a class="btn btn-primary btn-form display-4" target="_blank" href="http://www.123formbuilder.com/form-5581266/form">Contact Us Today!</a>
+
+                </div>
+                
+                <!-- <h3 class="mbr-section-subtitle align-center mbr-light pb-3 mbr-fonts-style display-5">
+                    Please fill out the form below and a specialist will get back to you shortly.
+                </h3> -->
+                
+            </div>
+        </div>
+    </div>
+    <!-- <div class="container" id="contact">
+        <div class="row justify-content-center">
+            <div class="media-container-column col-lg-8" data-form-type="formoid">
+                    <div data-form-alert="" hidden="">
+                        Thanks for filling out the form!
+                    </div>
+            
+                    <form class="mbr-form" action="https://pixelandpapers.com/" method="post" data-form-title="Contact Form"><input type="hidden" name="email" data-form-email="true" value="QlNVTwLJkWEidL5nLDeACQXdQUBzlLmmF9kXFQOiEZfY58aHgi5nmOMb6mIpT9eVWOArb67i+UZRYA5s8aK5tYp3gMBuK3ijagTCz0ZieAxp3wF8ExYgxpGarWoLMqTf" data-form-field="Email">
+                        <div class="row row-sm-offset">
+                            <div class="col-md-4 multi-horizontal" data-for="name">
+                                <div class="form-group">
+                                    <label class="form-control-label mbr-fonts-style display-7" for="name-form1-8">Name</label>
+                                    <input type="text" class="form-control" name="name" data-form-field="Name" required="" id="name-form1-8">
+                                </div>
+                            </div>
+                            <div class="col-md-4 multi-horizontal" data-for="email">
+                                <div class="form-group">
+                                    <label class="form-control-label mbr-fonts-style display-7" for="email-form1-8">Email</label>
+                                    <input type="email" class="form-control" name="email" data-form-field="Email" required="" id="email-form1-8">
+                                </div>
+                            </div>
+                            <div class="col-md-4 multi-horizontal" data-for="phone">
+                                <div class="form-group">
+                                    <label class="form-control-label mbr-fonts-style display-7" for="phone-form1-8">Phone</label>
+                                    <input type="tel" class="form-control" name="phone" data-form-field="Phone" id="phone-form1-8">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group" data-for="message">
+                            <label class="form-control-label mbr-fonts-style display-7" for="message-form1-8">Message</label>
+                            <textarea type="text" class="form-control" name="message" rows="7" data-form-field="Message" id="message-form1-8"></textarea>
+                        </div>
+            
+                        <span class="input-group-btn">
+                            <button href="" type="submit" class="btn btn-primary btn-form display-4">SEND FORM</button>
+                        </span>
+                    </form>
+            </div>
+        </div>
+    </div> -->
+</section>
+
+<!-- <section class="cid-rEFjgv9tqx mbr-reveal" id="footer5-b">
+
+    
+
+    
+
+    <div class="container">
+        <div class="media-container-row">
+            <div class="col-md-3">
+                <div class="media-wrap">
+                    <a href="https://mobirise.com/">
+                       <img src="wp-content/themes/pixelGenesis/content/assets/images/favicon-1-122x113.png" alt="Mobirise" title="">
+                    </a>
+                </div>
+            </div>
+            <div class="col-md-9">
+               
+            </div>
+        </div>
+        <div class="footer-lower">
+            <div class="left">
+                <div class="media-container-row">
+                    <div class="col-md-12">
+                        <hr>
+                    </div>
+                </div>
+                <div class="media-container-row mbr-white">
+                    <div class="col-md-6 copyright">
+                        <p class="mbr-text mbr-fonts-style display-7">
+                            © Copyright 2020 Pixel & Papers - All Rights Reserved
+                        </p>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="social-list align-right">
+                            
+                            <div class="soc-item">
+                                <a href="https://twitter.com/PapersPixel" target="_blank">
+                                    <span class="socicon-twitter socicon mbr-iconfont mbr-iconfont-social"></span>
+                                </a>
+                            </div>
+                            <div class="soc-item">
+                                <a href="https://www.facebook.com/Pixel-and-Papers-109608633753757/" target="_blank">
+                                    <span class="socicon-facebook socicon mbr-iconfont mbr-iconfont-social"></span>
+                                </a>
+                            </div>
+                            
+                            <div class="soc-item">
+                                <a href="https://www.linkedin.com/company/pixel-and-papers/" target="_blank">
+                                    <span class="socicon-linkedin socicon mbr-iconfont mbr-iconfont-social"></span>
+                                </a>
+                            </div>
+                            
+                            <div class="soc-item">
+                                <a href="https://www.instagram.com/pixel_and_papers/" target="_blank">
+                                    <span class="socicon-instagram socicon mbr-iconfont mbr-iconfont-social"></span>
+                                </a>
+                            </div>
+                            
+                          
+                            <div class="soc-item">
+                                <a href="https://plus.google.com/u/0/+Mobirise" target="_blank">
+                                    <span class="socicon-googleplus socicon mbr-iconfont mbr-iconfont-social"></span>
+                                </a>
+                            </div>
+                            <div class="soc-item">
+                                <a href="https://www.behance.net/Mobirise" target="_blank">
+                                    <span class="socicon-behance socicon mbr-iconfont mbr-iconfont-social"></span>
+                                </a>
+                            </div>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="right">
+               <?php //include 'partials/email-form-footer.php' ?>
+            </div>
+        </div>
+    </div>
+</section> -->
+
+
+<?php //include 'partials/footerscripts.php' ?>
+  
+  
+  <input name="animation" type="hidden">
+  </body>
+</html>
